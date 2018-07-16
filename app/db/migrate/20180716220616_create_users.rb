9cbcacde7b50fb281661b8ae8cc5a66759cb1092
Jeremie <jeremie.de-pas@epitech.eu>
2018-07-17 00:10:06 +0200

    create crud user

--- app/db/migrate/20180716220616_create_users.rb
+++ app/db/migrate/20180716220616_create_users.rb
@@ -1,11 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
+      t.date :birthedate
       t.string :lastname
       t.string :name
       t.string :password
       t.string :email
       t.text :description
+      t.string :roles
 
       t.timestamps
     end
--- app/db/schema.rb
+++ app/db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180716125305) do
+ActiveRecord::Schema.define(version: 20180716220616) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.date     "birthedate"
     t.string   "lastname"
     t.string   "name"
     t.string   "password"
