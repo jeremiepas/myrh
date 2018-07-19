cfe46bd153eaf026069a87f2f3f0316952ded820
Jeremie <jeremie.de-pas@epitech.eu>
2018-07-19 12:08:43 +0200

    create sessions controller

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
+
   resources :announces
   resources :cities
   resources :compagnies
   resources :responses
   resources :quizzes
   resources :users
+  resources :sessions
+  get '/inscription' => 'users#new'
+  get '/login'     => 'sessions#login'
+  post '/login'    => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  match ':controller(/:action(/:id))(.:format)', via: [:get]
 
 end
