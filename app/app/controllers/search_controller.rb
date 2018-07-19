359e8d5f5beceb3147745208b61d3d6a36afd6b0
Baptiste Chiocca <baptiste.chiocca@gmail.com>
2018-07-19 16:33:40 +0200

    Early version of view for search controller

--- app/app/controllers/search_controller.rb
+++ app/app/controllers/search_controller.rb
@@ -18,7 +18,12 @@ def search(&block)
     if params[:q]
     @results = yield if block_given?
 
-    render json: @results
+    respond_to do |format|
+        format.html { render :show }
+        format.json { head :no_content }
+    end
+
+    # render json: @results
     else
     redirect_to root_url, :notice => 'No search query was specified.'
     end
