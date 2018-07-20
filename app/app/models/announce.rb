a9e101f9bdc4bf3a423e99704396a69ecc894526
Edgear <nicolas.havard@epitech.eu>
2018-07-20 11:13:01 +0200

    View_announces finish

--- app/app/models/announce.rb
+++ app/app/models/announce.rb
@@ -2,5 +2,6 @@ class Announce < ApplicationRecord
   belongs_to :city, optional: false
   belongs_to :compagny, optional: false
   belongs_to :quiz, optional: false
+  accepts_nested_attributes_for :city, :compagny, :quiz
   validates :date, presence: true, optional: false
 end
